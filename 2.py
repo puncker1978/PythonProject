34ff1aa23e56f44baa5af83a78693d2478255b48
puncker1978 <nvv197827@gmail.com>
2025-03-28 14:16:28 +0500

    1

---
+++ 2.py
@@ -0,0 +1,22 @@
+# Задание 1
+name = input('Введите имя: ')
+print('Привет,', name)
+
+# Задание 2
+city = input('Город: ')
+hobby = input('Хобби: ')
+print('Я живу в городе', city, 'и люблю', hobby)
+
+# Задание 3
+first_name = input('Первое имя: ')
+second_name = input('Второе имя: ')
+third_name = input('Третье имя: ')
+
+print(third_name)
+print(second_name)
+print(first_name)
+
+# Задание 4
+dish = input('Блюдо: ')
+dessert = input('Десерт: ')
+print("На обед у меня", dish, "а на десерт", dessert)
